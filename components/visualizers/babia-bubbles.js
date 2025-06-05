71added82fc23eeb4e0a27adf815f25bc0d7aafc
Simao Soares <simaosoares798@gmail.com>
2025-06-05 17:35:37 +0100

    lables change in bubbles

--- components/visualizers/babia-bubbles.js
+++ components/visualizers/babia-bubbles.js
@@ -302,7 +302,7 @@ function generateBubble(height, radius, colorId, palette, positionX, positionZ, 
 }
 
 function generateLegend(data, bubble, bubbleEntity) {
-    let text = bubble[data.x_axis] + ': \n Radius:' + bubble[data.radius] + '\nHeight:' + bubble[data.height];
+    let text = bubble[data.z_axis] + bubble[data.x_axis] + '\nHeight:' + bubble[data.height];
 
     let width = 2;
     if (text.length > 16)
